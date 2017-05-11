8318a9459c1769d8d591eba87522ccdbb46eee6a
Wilhelm Klopp <wil.klopp@gmail.com>
2017-05-11 19:40:27 +0100

    Add Location component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -8,12 +8,14 @@ import Projects from './Projects';
 import SocialMediaIcons from './SocialMediaIcons';
 import Teams from './Teams';
 import Random from './Random';
+import Location from './Location';
 
 function App() {
   return (
     <div className="site-wrapper">
       <img src={face} className="face" />
       <h1 className="name">Wilhelm Klopp</h1>
+      <Location />
       <SocialMediaIcons />
       <Projects />
       <Teams />
---
+++ src/components/app/Location.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import './Location/styles.scss';
+
+const API_DOMAIN = 'https://api.wilhelmklopp.com';
+
+class Location extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: 'London, GB',
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${API_DOMAIN}/location`)
+    .then(response => response.json())
+    .then((json) => {
+      this.setState({ location: json.location });
+    });
+  }
+  render() {
+    return (
+      <div className="location">
+        <Choose>
+          <When condition={this.state.location === 'London, GB'}>
+            <span>{'Live in London, where I also currently am.'}</span>
+          </When>
+          <Otherwise>
+            <span>{`Live in London, but I'm currently in ${this.state.location}`}</span>
+          </Otherwise>
+        </Choose>
+      </div>
+    );
+  }
+}
+
+
+export default Location;
