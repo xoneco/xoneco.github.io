71adceeea3386e20374eefbb5f7a459587a618a6
Wilhelm Klopp <wil.klopp@gmail.com>
2017-05-28 23:14:03 +0100

    Integrate GitHub intern and add alt tag

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -9,14 +9,16 @@ import SocialMediaIcons from './SocialMediaIcons';
 import Teams from './Teams';
 import Random from './Random';
 import Location from './Location';
+import CurrentOccupation from './CurrentOccupation';
 
 function App() {
   return (
     <div className="site-wrapper">
-      <img src={face} className="face" />
+      <img src={face} className="face" alt="Wihelm Klopp face" />
       <h1 className="name">Wilhelm Klopp</h1>
       <Location />
       <SocialMediaIcons />
+      <CurrentOccupation />
       <Projects />
       <Teams />
       <Random />
